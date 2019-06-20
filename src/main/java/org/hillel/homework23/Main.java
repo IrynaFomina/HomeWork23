23b10da8f1da4e324de9509fdc755f5608632ea4
Irina Fomina <Irina.Paschenko@inforeachinc.com>
2019-06-20 17:51:11 +0300

    hw23

--- src/main/java/org/hillel/homework23/Main.java
+++ src/main/java/org/hillel/homework23/Main.java
@@ -3,34 +3,59 @@ package org.hillel.homework23;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(findPrimeNumbers(1, 1000).toString());
+        int poolSize = Runtime.getRuntime().availableProcessors();
+//        ThreadPoolExecutor poolExecutor = new ThreadPoolExecutor(poolSize, poolSize,60, TimeUnit.SECONDS,
+//                        )
+        long t = System.currentTimeMillis();
+        System.out.println(findPrimeNumbers(prepareData(1500000)).toString());
+//        System.out.println(prepareData(150).toString());
+        System.out.println(System.currentTimeMillis() - t);
     }
 
+    private static List<Integer> findPrimeNumbers(List<Integer> list) {
+        List<Integer> primeList = new ArrayList<>();
+        primeList.add(1);
+        for (int i = 1; i < list.size(); i++) {
+            boolean isPrime = true;
+            for (int j = 1; j < i; j++) {
+                if (list.get(i) % list.get(j) == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                primeList.add(list.get(i));
+            }
+        }
+        return primeList;
+    }
 
-    private static List<Integer> findPrimeNumbers(int min, int max) {
+    private static List<Integer> prepareData(int n) {
         List<Integer> list = new ArrayList<>();
-        for (int i = min; i < max; i++) {
-            if (i < 4) {
-                list.add(i);
-            } else if (i % 2 != 0) {
-                if (i % 3 != 0) {
-                    Iterator<Integer> it = list.iterator();
-                    boolean isPrime = true;
-                    while (it.hasNext()) {
-                        int value = it.next();
-                        if ( (i % value == 0) & value !=1) {
-                            isPrime = false;
-                            break;
-                        }
-                    }
-                    if (isPrime) list.add(i);
+        if (n > 1) {
+            list.add(1);
+        }
+        if (n > 2) {
+            list.add(2);
+        }
+        if (n > 3) {
+            list.add(3);
+
+            for (int i = 4; i <= n; i++) {
+                if ((i % 2) != 0
+                        & (i % 3) != 0
+                       ) { // & !((Math.sqrt(i) % 1) == 0)
+                    list.add(i);
                 }
             }
         }
         return list;
     }
-    }
+}
